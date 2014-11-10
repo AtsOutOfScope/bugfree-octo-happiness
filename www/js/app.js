73f9bd1f8c89e61177e14923edaa5f4c24acc607
nicolads87 <nicola.ds87@gmail.com>
2014-11-10 12:04:34 +0100

    gestito apertura documento

--- www/js/app.js
+++ www/js/app.js
@@ -42,18 +42,23 @@ app.controller('ContentController', function($scope, $ionicSideMenuDelegate ){
                
     /* DocumentHandler */
     $scope.openPdf = function() {
-               
-        /*handleDocumentWithURL(
-            function() {console.log('success');},
+        //$scope.cordova = window.cordova;
+        handleDocumentWithURL(
+            function() {
+                $scope.success = "success";
+                console.log('success');
+            },
             function(error) {
             console.log('failure');
+                $scope.error = "error" + error;
                 if(error == 53) {
                     console.log('No app that handles this file type.');
                 }
             },
-            '6.pdf'
-        );*/
-        window.open('6.pdf');
+            'http://www.repstatic.it/content/nazionale/img/2014/11/10/100921380-3773a7d0-495c-4238-a5b5-707794d2f95f.jpg'
+        );
+        //window.open('https://drive.google.com/open?id=0B-5YgTPghahqMEdSTm9TZGx4LVE&authuser=0');
+        //http://www.cheat-sheets.org/saved-copy/fwunixref.pdf
     }
     
     
