7c619109664f9e56bc78f9af53ba2ed76e74e201
nicolads87 <nicola.ds87@gmail.com>
2014-11-10 12:09:21 +0100

    Added servives

---
+++ www/js/services.js
@@ -0,0 +1,25 @@
+app.factory('Labels', function($http, $q) {
+    
+    
+    function handleSuccess(response) {
+        return response.data;
+    }
+    
+    function handleError(response) {
+        return $q.reject("An unknown error occurred.");
+    }
+    function get() {
+        var request = $http({
+            method: "get",
+            url: "http://localhost:8000/hello/ciao",
+            params: {
+                action: "get"
+            }
+        });
+        return(request.then(handleSuccess, handleError));
+    }
+    
+    return {
+        get: get
+    }
+});
