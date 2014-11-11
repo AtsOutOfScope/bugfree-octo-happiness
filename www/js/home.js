322766235ca02776eaba4b5d216950a1e72f91f3
nicolads87 <nicola.ds87@gmail.com>
2014-11-11 15:30:25 +0100

    index

--- www/js/home.js
+++ www/js/home.js
@@ -3,4 +3,12 @@ app.controller('homeController', function($scope) {
     vm.init = function() {
     };
     
+    $scope.langLabel = "ENG";
+    $scope.toggleLeft = function(index) {
+        $ionicSideMenuDelegate.toggleLeft();
+        if(angular.isDefined(index)){
+            $scope.$selected = index;
+        }
+    };
+    
 });
